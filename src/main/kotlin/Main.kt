7f68046aa7723ecdbec9fc4f7d8865b6e1b8447c
Idayam <mzeeidaya@gmail.com>
2023-03-09 08:55:10 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import java.lang.IndexOutOfBoundsException
+
+fun main(){
+    println(names())
+    println("book")
+    println(numbers())
+
+}
+fun names(){
+    var names=String;"Hidaya";"Rose"
+    println()
+
+}
+fun numbers(){
+    var numbers= arrayOf(4,5,86,35,78)
+    println(Int)
+
+}
+
+fun nice () {
+    var words= arrayOf("chair","books","shirts","jackets")
+    println(words)
+
+}
+
